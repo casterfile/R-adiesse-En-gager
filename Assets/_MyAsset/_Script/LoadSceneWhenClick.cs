f35547205abb79f8e3bf1cc9a24bb51b9d6a0f45
tonyMac <tonyMac@tonys-MacBook-Pro.local>
2019-12-05 13:56:01 +0800

    first commit2

---
+++ Assets/_MyAsset/_Script/LoadSceneWhenClick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneWhenClick : MonoBehaviour {
+
+    public void LoadA(string scenename)
+    {
+        Debug.Log("sceneName to load: " + scenename);
+        SceneManager.LoadScene(scenename);
+    }
+}
